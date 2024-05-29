afbf8ec506d59ec99234942dbc4ecb75fda07594
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2024-05-29 06:21:41 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+//Async Fuction
+console.log("Thise are Promises");
+
+// let prom1 = new Promise();
+
+const callback = (args) => {
+  console.log("This is Callback function");
+};
+
+let prom1 = new Promise((resolve, reject) => {
+  let a = Math.random();
+  if (a < 0.5) {
+    reject("Unable to load promised functions");
+  } else {
+    setTimeout(() => {
+      console.log("Promised functions are loaded successfully");
+      resolve("Resolved");
+    }, 3000);
+  }
+});
+
+prom1
+  .then((a) => {
+    console.log(a);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const loadscript = (src, callback) => {
+  let sc = document.createElement("script");
+  sc.onload = callback("hello");
+  document.head.append(sc);
+};
+
+loadscript(
+  "https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js",
+  callback
+);
